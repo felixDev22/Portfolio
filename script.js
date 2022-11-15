08f8161eb6bd2e6afbc71c0c68f07d09bebcc169
Felix <felixnzomo@gmail.com>
2022-11-15 17:14:15 +0300

    Add html-css-js linters file, Add script.js file

---
+++ script.js
@@ -0,0 +1,11 @@
+const mobileMenu = document.querySelector(".mobile-menu");
+const menuBar = document.querySelector(".menubar")
+const closeBtn = document.querySelector(".menu-x");                                    
+
+
+function closeMenu() {
+  mobileMenu.
+}
+
+
+
