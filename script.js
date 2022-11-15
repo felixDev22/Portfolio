1a7317660f108f086796f380082c248826fdb6bb
Felix <felixnzomo@gmail.com>
2022-11-15 20:30:34 +0300

    Update the size of menu hamburger

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ const mobileMenu = document.getElementById("mobile-menu");
 const closeBtn = document.getElementById("closeMenu");
 const menu = document.getElementById("menu");
 
-
-
 menu.onclick = function showMenu(){
   mobileMenu.style.display='block';
 };
