aef12c030ab48d871af9ae54a0d8bf7789b8ee13
Felix <felixnzomo@gmail.com>
2022-11-19 11:23:53 +0300

    Add js function to collect and preserve data for the form section

--- script.js
+++ script.js
@@ -407,3 +407,19 @@ const Form = document.getElementById("form");
 const Name = document.getElementById("name");
 const Email = document.getElementById("email");
 const Message = document.getElementById("message");
+
+contactForm.addEventListener('input', () => {
+  const contactData = {
+    username: Name.value,
+    email: Email.value,
+    message: Message.value,
+  };
+  localStorage.setItem('userData', JSON.stringify(contactData));
+});
+
+const GetDataFromLocalStorage = JSON.parse(localStorage.getItem('userData'));
+Name.value = GetDataFromLocalStorage.username;
+Email.value = GetDataFromLocalStorage.email;
+Message.value = GetDataFromLocalStorage.message;
+
+
